ede9d9a8884260e58b3877e47823c2c15a9c95bd
Moritz <moritz.rfs@gmail.com>
2023-02-09 18:35:02 +0100

    Add scraper logic

--- scraper.py
+++ scraper.py
@@ -1,42 +1,74 @@
 from bs4 import BeautifulSoup
 import requests
+from utils.split_time import split_time, extract_date
+from datetime import datetime, date, time
+from utils.encoding import encode_to_utf8
+from lecture import Lecture
 
-class Lecture:
-    def __init__(self, name, time, room):
-        self.name = name
-        self.time = time
-        self.room = room
+class Scraper:
+    def __init__(self, url: str):
+        self.url = url
 
-    def __str__(self):
-        return f'{self.name} {self.time} {self.room}'
+    def get_soup(self):
+        html = requests.get(self.url).text
+        soup = BeautifulSoup(html, 'html.parser')
+        return soup
 
+    def get_data(self):
+        data = self.get_soup()
+        days = data.find('div', {'class': 'ui-grid-e'})
+        return days
 
-def get_soup(url):
-    """Returns the soup of a url"""
-    html = requests.get(url).text
-    soup = BeautifulSoup(html, 'html.parser')
-    return soup
+    def lectures_json(self):
+        lectures = self.get_lectures()
+        lectures_json = []
+        for lecture in lectures:
+            lecture_json = {
+                "name": lecture.name,
+                "date": lecture.date.strftime("%d.%m.%Y"),
+                "start_time": lecture.start_time.strftime("%H:%M"),
+                "end_time": lecture.end_time.strftime("%H:%M"),
+                "room": lecture.room,
+                "info": lecture.info
+            }
+            lectures_json.append(lecture_json)
+        return lectures_json
 
-data = get_soup('https://vorlesungsplan.dhbw-mannheim.de/index.php?action=view&gid=3067001&uid=8064001')
-divs = data.find('div', {'class': 'ui-grid-e'})
-lectures = []
+    def get_lectures(self) -> list[Lecture]:
+        """
+        Returns a a list ob lectures with type Lecture
+        """
+        lectures = []
+        days = self.get_data()
+        for day in days:
+            ul = day.find('ul')
+            try:
+                lis = ul.find_all('li')
+            except AttributeError:
+                continue
+            lecture_date = lis[0].text
+            lecture_date = extract_date(lecture_date)
+            for li in lis[1:]:
+                try:
+                    lecture_cal_time = li.find('div', {'class': 'cal-time'}).text
+                    lecture_cal_title = encode_to_utf8(li.find('div', {'class': 'cal-title'}).text)
+                    lecture_cal_room = li.find('div', {'class': 'cal-res'})
+                    lecture_cal_text = li.find('div', {'class': 'cal-text'})
+                except AttributeError:
+                    continue
 
-for div in divs:
-    ul = div.find('ul')
-    lis = ul.find_all('li')
-    lecture_date = lis[0].text
-
-
-    for li in lis[1:]:
-        lecture_cal_time = li.find('div', {'class': 'cal-time'})
-        lecture_cal_title = li.find('div', {'class': 'cal-title'})
-        lecture_cal_room = li.find('div', {'class': 'cal-res'})
-        print(lecture_cal_time.text)
-
+                if lecture_cal_text:
+                    lecture_cal_text = encode_to_utf8(lecture_cal_text.text)
+                else:
+                    lecture_cal_text = None
+                
+                if lecture_cal_room:
+                    lecture_cal_room = encode_to_utf8(lecture_cal_room.text)
+                else:
+                    lecture_cal_room = None
+                
+                start_time, end_time = split_time(lecture_cal_time)
+                lecture = Lecture(lecture_cal_title, lecture_date, start_time, end_time, lecture_cal_room, lecture_cal_text)
+                lectures.append(lecture)
     
-
-    _lecture = Lecture("Temp", lecture_date, "Temp")
-    lectures.append(_lecture)
-
-for lecture in lectures:
-    print(lecture)
+        return lectures
